7b20bde4657ced0108f1753803dc4244c20ebcac
Donny K R <61267430+naminai@users.noreply.github.com>
2020-04-03 14:41:26 +0700

    Mantap jiwa

---
+++ soal3/soal3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <pthread.h>
